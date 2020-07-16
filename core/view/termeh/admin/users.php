380c7f2651dc54584a71d6b266fc9c2fb7d2aa8b
Milad Abooali <m.abooali@hotmail.com>
2020-07-16 17:13:48 +0430

    initial

--- core/view/termeh/admin/users.php
+++ core/view/termeh/admin/users.php
@@ -1,2 +1,21 @@
 <?php
+    /**
+     * admin/users
+     *
+     * Mahan | View File
+     *
+     * @package    App
+     * @author     Milad Abooali <m.abooali@hotmail.com>
+     * @copyright  2012 - 2020 Codebox
+     * @license    http://codebox.ir/license/1_0.txt  Codebox License 1.0
+     * @version    1.0.0
+     */
+?>
 
+<?php
+    App\Core\M::print($this->page['data']);
+?>
+
+<br> ------ <br>
+Admin/users
+<br> ------ <br>
