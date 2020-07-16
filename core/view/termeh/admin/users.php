788be7dd0abf4d3121f8258c4e6cb3aa04d8057c
Milad Abooali <m.abooali@hotmail.com>
2020-07-16 19:06:27 +0430

    Users

--- core/view/termeh/admin/users.php
+++ core/view/termeh/admin/users.php
@@ -1,21 +1,59 @@
 <?php
-    /**
-     * admin/users
-     *
-     * Mahan | View File
-     *
-     * @package    App
-     * @author     Milad Abooali <m.abooali@hotmail.com>
-     * @copyright  2012 - 2020 Codebox
-     * @license    http://codebox.ir/license/1_0.txt  Codebox License 1.0
-     * @version    1.0.0
-     */
+
+    $this->data['PAGE']['demo']=0;
+
+    $this->data['PAGE']['title'] = 'A > Users';
+    $this->data['PAGE']['head'] = ' ';
+
+    include('core/view/termeh/head.php');
+    include('core/view/termeh/header.php');
 ?>
+
+    <div id="app-body" class="container" data-g="home">
+
+        <!-- Row Map -->
+        <div id="cb-map" class="row cb-oh">
+
+        </div>
+
+        <!-- Row Domain -->
+        <div id="cb-domian" class="card mt-4 pt-4 d-none d-md-block cb-oh">
+
+        </div>
+
+
+        <!-- Row Hosting Tip -->
+        <div id="cb-host-tip" class="cb-blg-0 my-4 pt-2 cb-oh">
+
+        </div>
+
+        <!-- Row CMS -->
+        <div id="cb-cms" class="row mb-5 cb-oh">
+
+        </div>
+
+        <!-- Row Puls -->
+        <div id="cb-dev" class="row mt-5 mb-4 mx-1 py-5 px-3 cb-blg-4 cb-rtl cb-oh" dir="rtl">
+
+        </div>
+
+        <!-- Row VPS Tip -->
+        <div id="cb-vps-tip" class="cb-blg-5 mb-5 pt-2 cb-oh">
+
+        </div>
+
+        <!-- Row Content -->
+        <div id="cb-posts" class="row mb-5 d-md-down-none cb-oh">
+
+        </div>
+
+        <!-- Row Staff -->
+        <div id="cb-staff" class="row my-5 mx-1 pt-5 px-3 cb-blg-0  d-none d-md-flex cb-oh">
+
+        </div>
+
+    </div>
 
 <?php
-    App\Core\M::print($this->data);
+    include('core/view/termeh/footer.php');
 ?>
-
-<br> ------ <br>
-Admin/users
-<br> ------ <br>
--- core/view/termeh/head.php
+++ core/view/termeh/head.php
@@ -15,57 +15,12 @@
   <!--- Prefetch 
     <link rel='dns-prefetch' href="<?= CDN ?>" />
   -->
-  <!--- Verification -->
-    <meta name="yandex-verification" content="8180440969c4b9fe" />
-    <meta name="p:domain_verify" content="0844a5db1e5d837d475aeea91975fa85" />
   <!--- SEO -->
     <title><?= $this->data['PAGE']['title']; ?> &bull; Codebox</title>
-    <meta name="keywords" content="<?= $this->data['PAGE']['keywords']; ?>" />
-    <meta name="description" content="<?= $this->data['PAGE']['description']; ?>" />
-    <meta name="robots" content="<?= ($this->data['PAGE']['robots'])?'noindex, nofollow':'index, follow'; ?>" />
-  <!--- Open Graph -->
-    <meta property="og:locale" content="fa_IR" />
-    <meta property="og:type" content="article" />
-    <meta property="og:site_name" content="Codebox" />
-    <meta property="og:image" content="<?= ($this->data['PAGE']['image']) ? $this->data['PAGE']['image'] : IMG.'og.jpg'; ?>" />
-    <meta property="og:description" content="<?= $this->data['PAGE']['description']; ?>" />
-    <meta property="og:APP_URL" content="<?= APP_URL ?><?= $this->data['PAGE']['path']; ?>" />
-    <meta property="og:title" content="<?= $this->data['PAGE']['title']; ?> &bull; Codebox" />
-    <meta property="article:publisher" content="https://www.facebook.com/codebox.ir/" />
-  <!--- Twitter Card -->
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@codbox_ir" />
-    <meta name="twitter:creator" content="@codbox_ir" />
-    <meta name="twitter:image" content="<?= ($this->data['PAGE']['image']) ? $this->data['PAGE']['image'] : IMG.'og.jpg'; ?>" />
-    <meta name="twitter:description" content="<?= $this->data['PAGE']['description']; ?>" />
-    <meta name="twitter:title" content="<?= $this->data['PAGE']['title']; ?> &bull; Codebox" />
+    <meta name="robots" content="noindex, nofollow" />
   <!--- ICO -->
     <meta name="application-name" content="Codebox.ir" />
     <link rel="icon" type="image/ico" sizes="32x32" href="<?= ICO ?>favicon.ico" />
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= ICO ?>favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= ICO ?>favicon-16x16.png" />
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= ICO ?>favicon-96x96.png" />
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?= ICO ?>android-icon-192x192.png" />
-  <!--- Microsoft -->
-    <meta name="theme-color" content="#fff" />
-    <link rel="manifest" href="<?= ICO ?>manifest.json" />
-    <meta name="msapplication-TileColor" content="#ffffff" />
-    <meta name="msapplication-TileImage" content="<?= ICO ?>ms-icon-144x144.png" />
-  <!--- Apple -->
-    <meta name="format-detection" content="telephone=no" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= ICO ?>apple-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= ICO ?>apple-icon-60x60.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= ICO ?>apple-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= ICO ?>apple-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= ICO ?>apple-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= ICO ?>apple-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= ICO ?>apple-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= ICO ?>apple-icon-152x152.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= ICO ?>apple-icon-180x180.png" />
-  <!--- Alternative -->
-    <link rel="canonical" href="<?= ($this->data['PAGE']['canonical']) ? $this->data['PAGE']['canonical'] : APP_URL; ?>" />
   <!--- CSS -->
     <link href="<?= CSS ?>style.css" rel="stylesheet" />
   <!--- Script -->
