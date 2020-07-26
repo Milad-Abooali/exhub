70a935b89581ef9b32ce1d8ce2d4c46773a01791
Milad Abooali <m.abooali@hotmail.com>
2020-07-26 20:53:36 +0430

    ExJS

--- core/ajax/seo.php
+++ core/ajax/seo.php
@@ -24,50 +24,56 @@
         echo json_encode($output);
     }
 
-    /**
-     * Set keywords priority
-     */
-    function setPrio () {
-        $db = new MySQL(DB_INFO,'seo_keywords');
-        $output = new stdClass();
-        $id = ($_POST['rid']) ?? die();
-        $update['priority'] = ($_POST['prio']) ?? 0;
-        $res = $db->updateId($id, $update);
-        $output->e = ($res) ? false : true;
-        $output->res = $user->ERROR ?? true;
-        global $actlog;
-        $actlog->add("Set Priority for keyword ($id)",$update['priority'], $id,($output->res)?1:0);
-        echo json_encode($output);
-    }
+    if ($_SESSION['M']['user']) {
 
-    /**
-     * Set keywords fis
-     */
-    function setFis () {
-        $db = new MySQL(DB_INFO,'seo_keywords');
-        $output = new stdClass();
-        $id = ($_POST['rid']) ?? die();
-        $update['fis'] = ($_POST['fis']) ? 1 : 0;
-        $res = $db->updateId($id, $update);
-        $output->e = ($res) ? false : true;
-        $output->res = $user->ERROR ?? true;
-        global $actlog;
-        $actlog->add("Set Fis for keyword ($id)",$update['fis'], $id,($output->res)?1:0);
-        echo json_encode($output);
-    }
+        /**
+         * Set keywords priority
+         */
+        function setPrio()
+        {
+            $db = new MySQL(DB_INFO, 'seo_keywords');
+            $output = new stdClass();
+            $id = ($_POST['rid']) ?? die();
+            $update['priority'] = ($_POST['prio']) ?? 0;
+            $res = $db->updateId($id, $update);
+            $output->e = ($res) ? false : true;
+            $output->res = $user->ERROR ?? true;
+            global $actlog;
+            $actlog->add("Set Priority for keyword ($id)", $update['priority'], $id, ($output->res) ? 1 : 0);
+            echo json_encode($output);
+        }
 
-    /**
-     * Set Add fis
-     */
-    function addFis () {
-        $db = new MySQL(DB_INFO,'seo_fis');
-        $_POST['user_id'] = $_SESSION['M']['user']['id'];
-        $res = $db->insert($_POST);
-        $output = new stdClass();
-        $output->e = ($res) ? false : true;
-        $output->res = ($res) ?? false;
-        global $actlog;
-        $actlog->add("Fis on ".$_POST['engin'],null,($_POST['keyword_id']) ?? null,($output->res)?1:0);
-        echo json_encode($output);
-    }
+        /**
+         * Set keywords fis
+         */
+        function setFis()
+        {
+            $db = new MySQL(DB_INFO, 'seo_keywords');
+            $output = new stdClass();
+            $id = ($_POST['rid']) ?? die();
+            $update['fis'] = ($_POST['fis']) ? 1 : 0;
+            $res = $db->updateId($id, $update);
+            $output->e = ($res) ? false : true;
+            $output->res = $user->ERROR ?? true;
+            global $actlog;
+            $actlog->add("Set Fis for keyword ($id)", $update['fis'], $id, ($output->res) ? 1 : 0);
+            echo json_encode($output);
+        }
 
+        /**
+         * Set Add fis
+         */
+        function addFis()
+        {
+            $db = new MySQL(DB_INFO, 'seo_fis');
+            $_POST['user_id'] = $_SESSION['M']['user']['id'];
+            $res = $db->insert($_POST);
+            $output = new stdClass();
+            $output->e = ($res) ? false : true;
+            $output->res = ($res) ?? false;
+            global $actlog;
+            $actlog->add("Fis on " . $_POST['engin'], null, ($_POST['keyword_id']) ?? null, ($output->res) ? 1 : 0);
+            echo json_encode($output);
+        }
+
+    }
