b35dcbb7372ee3be8d1462500263b682d979ebab
Milad Abooali <m.abooali@hotmail.com>
2020-08-07 06:03:25 +0430

    IPS ADD IP

--- core/ajax/ipt.php
+++ core/ajax/ipt.php
@@ -34,18 +34,38 @@
         {
             $table = 'ipt_ips';
             $db = new MySQL(DB_INFO, $table);
-            if ($_POST['ip4']>$_POST['ip5']) {
 
+            $data['ip'] = $_POST['ip1'].'.'.$_POST['ip2'].'.'.$_POST['ip3'].'.';
+            $data['mac'] = $_POST['mac'];
+            $data['network_id'] = $_POST['network_id'];
+            $data['country'] = $_POST['country'];
+            $data['flag'] = $_POST['flag'];
+            $data['ir_access'] = $_POST['ir_access'];
+            $data['ir_block'] = $_POST['ir_block'];
+            $data['abuse'] = $_POST['abuse'];
+            $data['owner'] = $_POST['owner'];
+            $data['note'] = $_POST['note'];
+            $data['status'] = 1;
+
+            if ($_POST['ip5']>$_POST['ip4']) {
+                $i = $_POST['ip5']-$_POST['ip4'];
+                while ($i>0) {
+
+                }
             } else {
-
+                $ip = $data['ip'] .= $_POST['ip4'];
+                if ($db->exist("ip='$ip'")) {
+                    $res = false;
+                } else {
+                    $res = $db->insert($data);
+                }
+                $output = new stdClass();
+                $output->e = ($res) ? false : true;
+                $output->res = ($res) ?? false;
+                global $actlog;
+                $actlog->add("Add Item to ($table)", $data, ($res) ?? null, (isset($res)) ? 1 : 0);
+                echo json_encode($output);
             }
-            $res = $db->insert($_POST);
-            $output = new stdClass();
-            $output->e = ($res) ? false : true;
-            $output->res = ($res) ?? false;
-            global $actlog;
-            $actlog->add("Add Item to ($table)", $_POST, ($res) ?? null, (isset($res)) ? 1 : 0);
-            echo json_encode($output);
         }
 
     }
